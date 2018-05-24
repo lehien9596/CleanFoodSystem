384d178582fac6f9ae9ef9002afb8e2f6d70a2d9
bactd-java <bactd@ominext.com>
2018-05-24 22:43:25 +0700

    update

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -29,9 +30,23 @@ public class LoginController extends HttpServlet {
 		UserModel userModel = new UserModel();
 		userModel.setNameUser(username);
 		userModel.setPassword(password);
-		boolean check = new LoginDAO().isLogin(userModel);
-		if (check) {
-			RequestDispatcher rd2 = request.getRequestDispatcher("trangchu.jsp");
+		Map<String, Integer> map = new LoginDAO().login(userModel);
+		if (!map.isEmpty()) {
+			int role = map.get("role");
+			RequestDispatcher rd2 = null;
+			switch (role) {
+			case 0:
+				rd2 = request.getRequestDispatcher("trangchu.jsp");
+				break;
+			case 1 :
+				//call provider
+				rd2 =request.getRequestDispatcher("/provider");
+				break;
+			case 2 :
+				//Em làm tiếp nhé, anh mệt quá đi ngủ đây =)))
+			default:
+				break;
+			}
 			rd2.forward(request, response);
 		} else {
 			RequestDispatcher rd3 = request.getRequestDispatcher("erorr_login.jsp");
--- src/main/java/dao/LoginDAO.java
+++ src/main/java/dao/LoginDAO.java
@@ -3,38 +3,42 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
 
 import connector.MysqlConnector;
 import model.UserModel;
 
 public class LoginDAO {
-	private Connection connect =null;
-	 private MysqlConnector instance=MysqlConnector.getInstance();
-	 public boolean isLogin (UserModel user) {
-		 PreparedStatement pstm=null;
-		 ResultSet rs=null;
-		 try {
-			 connect=instance.getDataBaseConnectionPool();
-			 pstm=connect.prepareStatement("SELECT nameUser, password FROM user WHERE nameUser =? AND password =?");
-			 pstm.setString(1, user.getNameUser());
-			 pstm.setString(2, user.getPassword());
-			 rs=pstm.executeQuery();
-			 while (rs.next()) {
-				 String username= rs.getString("nameUser");		
-			     String password=rs.getString("password");
-			     if (username.equals(user.getNameUser()) && password.equals(user.getPassword())){
-			    	 return true;
-			     }
-			 }
-		 }
-		 catch (Exception e) {
+	private Connection connect = null;
+	private MysqlConnector instance = MysqlConnector.getInstance();
+
+	public Map<String, Integer> login(UserModel user) {
+		Map<String, Integer> map = new HashMap<>();
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		try {
+			connect = instance.getDataBaseConnectionPool();
+			pstm = connect
+					.prepareStatement("SELECT nameUser, password, role FROM user WHERE nameUser =? AND password =?");
+			pstm.setString(1, user.getNameUser());
+			pstm.setString(2, user.getPassword());
+			rs = pstm.executeQuery();
+			while (rs.next()) {
+				String username = rs.getString("nameUser");
+				String password = rs.getString("password");
+				if (username.equals(user.getNameUser()) && password.equals(user.getPassword())) {
+					map.put("role", rs.getInt("role"));
+					break;
+				}
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		 finally {
+		} finally {
 			instance.closeResulset(rs);
 			instance.closePrepareStatement(pstm);
-	        instance.closeConnection(connect);
-		 }
-		return false;
-	 }
+			instance.closeConnection(connect);
+		}
+		return map;
 	}
+}
